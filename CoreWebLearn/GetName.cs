b83e2972512308fbd6301c1c2d1f444e385b4c25
18010105 <18010105@PRCNMG1L0311>
2018-01-25 11:51:14 +0800

    添加项目文件。

---
+++ CoreWebLearn/GetName.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWebLearn
+{
+    public class GetName : IGetName
+    {
+        public string Name => "Tim lv(吕明鸿)";
+    }
+}
---
+++ CoreWebLearn/HelloMiddle.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreWebLearn
+{
+    public class HelloMiddle
+    {
+        public RequestDelegate _next;
+        public string _content;
+        public string _contentType;
+        public IGetName _getName;
+        public HelloMiddle(RequestDelegate next,IGetName getName,string content,string contentType)
+        {
+            _next = next;
+            _getName = getName;
+            _content = content;
+            _contentType = contentType;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            context.Response.ContentType = _contentType;
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            var byteContent = Encoding.GetEncoding("GB2312").GetBytes(_content + _getName.Name);
+            await context.Response.Body.WriteAsync(byteContent, 0, byteContent.Length);
+        }
+    }
+}
