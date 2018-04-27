9f16cd2a650dfdb7df04005e18492230a649bc31
tim <1510899867@qq.com>
2018-04-27 17:53:43 +0800

    Oauth

---
+++ OAuthImplement/Startup.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OAuthImplement.Data;
+using OAuthImplement.Models;
+using OAuthImplement.Services;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.Extensions.Options;
+
+namespace OAuthImplement
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
+            services.AddAuthentication().AddGoogle(options =>
+            {
+                options.ClientId = "24324-12342-234-343--3412";
+                options.ClientSecret = "13213-24-32423-3423-234-23";
+            })
+            .AddMicrosoftAccount(options =>
+            {
+                options.ClientId = "1234124124-12341324-234";
+                options.ClientSecret = "132423";
+            });
+            //services.Configure<GoogleOptions>(options =>
+            //{
+            //    options.ClientId = "24324-12342-234-343--3412";
+            //});
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseBrowserLink();
+            //    app.UseDeveloperExceptionPage();
+            //    app.UseDatabaseErrorPage();
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Home/Error");
+            //}
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
