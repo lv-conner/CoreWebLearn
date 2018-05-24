9762332b28a7503dc951fe238e4128e271aa5d73
tim <1510899867@qq.com>
2018-05-24 14:36:09 +0800

    测试cookie

---
+++ CookieAuthentication/Controllers/CookieController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookieAuthentication.Controllers
+{
+    public class CookieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Set(string key,string value)
+        {
+            HttpContext.Response.Cookies.Append(key, value, new CookieOptions()
+            {
+                HttpOnly = true,
+                Secure = false,
+                Path = "/"
+            });
+            return Redirect("/Cookie/Get?key=" + key);
+        }
+
+        public IActionResult Get(string key)
+        {
+            HttpContext.Request.Cookies.TryGetValue(key, out var value);
+            return Redirect("/Home/Index");
+        }
+    }
+}
